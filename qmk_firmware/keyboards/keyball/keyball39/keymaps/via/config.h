2d359ffd8b81430d471f62fe94d7a2870947d103
tamura55 <na0k1kuwa71@yahoo.co.jp>
2024-08-27 22:23:29 +0900

    Tapping Termを200→250へ変更

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -40,7 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_DEFAULT_LAYER 6  // AMLはレイヤー6にする
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7  // レイヤー数を7へ増加
 #define AUTO_MOUSE_LAYER_KEEP_TIME 30000  // AML移行後は30s保持
-#define TAPPING_TERM 200  // 追加
+#define TAPPING_TERM 250  // 200→250へ変更
 #define PERMISSIVE_HOLD  //追加
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY  // 追加
 #define RETRO_TAPPING_PER_KEY  // 追加
