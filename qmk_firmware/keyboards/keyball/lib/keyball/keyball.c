66680442a8c940a9bfdb9243588bd939248ebbd3
tamura55 <na0k1kuwa71@yahoo.co.jp>
2024-09-10 13:11:14 +0900

    修正トライ

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
@@ -726,8 +726,6 @@ bool is_mouse_record_kb(uint16_t keycode, keyrecord_t* record) {
     // 押下後もAML保持したいキーを定義
     switch (keycode) {
         case SCRL_MO:
-//        case KC_MS_BTN4:  // 多分記載不要
-//        case KC_MS_BTN5:  // 多分記載不要
         case RCTL_T(KC_GRV):  // 親指キー。L6スぺ
         case RSFT_T(KC_SPC):  // 親指キー。L6スぺ
         case RCS_T(KC_ESC):  // 親指キー。L6スぺ
@@ -777,13 +775,11 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
             case KC_MS_BTN1:
             case KC_MS_BTN2:
             case KC_MS_BTN3:
-            case RCTL_T(KC_GRV):  // 親指キー。L6スぺ
-            case RSFT_T(KC_SPC):  // 親指キー。L6スぺ
-            case RCS_T(KC_ESC):  // 親指キー。L6スぺ
-//            case LT(2,KC_TAB):  // 親指キー。L0共通
-//            case LT(1,KC_ENT):  // 親指キー。L0共通
-            case RALT_T(KC_ESC):  // 親指キー予備。L6スぺ
-            case RALT_T(KC_MINS):  // 小指付け根キー。L6スぺ
+//            case RCTL_T(KC_GRV):  // 親指キー。L6スぺ
+//            case RSFT_T(KC_SPC):  // 親指キー。L6スぺ
+//            case RCS_T(KC_ESC):  // 親指キー。L6スぺ
+//            case RALT_T(KC_ESC):  // 親指キー予備。L6スぺ
+//            case RALT_T(KC_MINS):  // 小指付け根キー。L6スぺ
                 set_auto_mouse_timeout(keyball_get_auto_mouse_timeout());
                 keyball.total_mouse_movement = 0;
         }
