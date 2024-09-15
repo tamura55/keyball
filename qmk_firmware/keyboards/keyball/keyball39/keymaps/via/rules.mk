f3343ad0d2a3791335118908060daf8256fc24ba
tamura55 <na0k1kuwa71@yahoo.co.jp>
2024-09-15 23:42:15 +0900

    RGB Matrixにて一部点灯トライ

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/rules.mk
@@ -1,7 +1,13 @@
-RGBLIGHT_ENABLE = yes
+RGBLIGHT_ENABLE = no  # RGB Matrix有効にあたり無効化
+
+# RGB Matrix用の設定
+RGB_MATRIX_ENABLE = yes
+# Optional: power saving and other RGB Matrix features
+RGB_MATRIX_KEYPRESSES = yes  # キー押下に応じて反応するRGBモード
+RGB_MATRIX_FRAMEBUFFER_EFFECTS = yes  # フレームバッファを使う効果を有効化
 
 OLED_ENABLE = yes
 
 VIA_ENABLE = yes
 
-DYNAMIC_TAPPING_TERM_ENABLE = yes  // 追記
+DYNAMIC_TAPPING_TERM_ENABLE = yes  # 追記
