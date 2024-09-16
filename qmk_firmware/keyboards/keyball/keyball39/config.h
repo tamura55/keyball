5ff0db7c42ad9798e6bab3337a2b8843f81d6491
tamura55 <na0k1kuwa71@yahoo.co.jp>
2024-09-16 11:00:01 +0900

    範囲制御用設定をオミット＆明るさ上限200へ変更

--- qmk_firmware/keyboards/keyball/keyball39/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/config.h
@@ -47,10 +47,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLED_NUM      48
 #    define RGBLED_SPLIT    { 24, 24 }  // (24 + 22)
-#    define RGBLIGHT_LAYERS  // LED範囲制御にあたり追加
-#    define SPLIT_LAYER_STATE_ENABLE  // LED範囲制御にあたり追加
+// #    define RGBLIGHT_LAYERS  // LED範囲制御にあたり追加
+// #    define SPLIT_LAYER_STATE_ENABLE  // LED範囲制御にあたり追加
 #    ifndef RGBLIGHT_LIMIT_VAL
-#        define RGBLIGHT_LIMIT_VAL  150 // limitated for power consumption
+#        define RGBLIGHT_LIMIT_VAL  200 // limitated for power consumption
 #    endif
 #    ifndef RGBLIGHT_VAL_STEP
 #        define RGBLIGHT_VAL_STEP   15
