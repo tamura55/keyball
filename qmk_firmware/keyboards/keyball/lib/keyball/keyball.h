02172d0d59c036db573b6001f569adc25f643890
tamura55 <na0k1kuwa71@yahoo.co.jp>
2024-10-10 19:52:06 +0900

    TD_IME3へ名称変更

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.h
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.h
@@ -124,9 +124,9 @@ enum keyball_keycodes {
     STSP_HOR = QK_KB_24, // Set scroll snap mode as horizontal
     STSP_FRE = QK_KB_25, // Set scroll snap mode as disable (free scroll)
 
-    EXL_FLT = QK_KB_26,  // Excelフィルタ用
-    MHEN_CW = QK_KB_27,  // Caps Word用
-    IME_CTL = QK_KB_28,  // IME切替Tap Dance
+    EXL_FLT  = QK_KB_26, // Excelフィルタ用
+    MHEN_CW  = QK_KB_27, // Caps Word用
+    TD_IME3  = QK_KB_28, // IME切替Tap Dance
     
     // 親指・小指付け根キーを定義
 //    THM_LL = LCTL_T(KC_INT4),  // 変換
