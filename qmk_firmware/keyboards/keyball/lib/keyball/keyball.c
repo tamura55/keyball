34951e936c155fc9360e3e6c15bf84052b0dd1d9
tamura55 <na0k1kuwa71@yahoo.co.jp>
2025-02-15 17:10:23 +0900

    #include "send_string.h"をコメントアウト

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <string.h>
 
-#include "send_string.h" // 追加。SEND_STRING を使う場合に必要
+//#include "send_string.h" // 追加。SEND_STRING を使う場合に必要
 
 const uint8_t CPI_DEFAULT    = KEYBALL_CPI_DEFAULT / 100;
 const uint8_t CPI_MAX        = pmw3360_MAXCPI + 1;
